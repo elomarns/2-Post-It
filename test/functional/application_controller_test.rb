b81c2c66dfe9f42beca551df5cc69df7f359b2e2
Elomar Nascimento dos Santos <elomarns@gmail.com>
2009-10-31 11:21:29 -0200

    Adding the functional test of website index page.

---
+++ test/functional/application_controller_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class ApplicationControllerTest < ActionController::TestCase
+
+  test "should get website home" do
+    get :index
+
+    assert_template "index"
+    assert_response :success
+  end
+
+  # TODO: test the rendering of entire website index page (not only the sign in form).
+
+  test "should show sign in form" do
+    get :index
+
+    assert_select "form", :count => 1
+    assert_select "form > p", :count => 4
+    assert_select "form > p > input", :count => 4
+    assert_select "form > p > input[type=text]", :count => 1
+    assert_select "form > p > input[type=password]", :count => 1
+    assert_select "form > p > input[type=submit]", :count => 1
+    assert_select "form > p > input[type=hidden]", :count => 1
+  end
+end
